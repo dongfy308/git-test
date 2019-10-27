50070933c8e364aa0515ecd8ec59d0b2fabb22ef
zhangsan <zhangsan@itcast.cn>
2019-10-27 21:31:59 +0800

    把本地工程添加到git

--- git-test/git01/src/com/itheima/git/GitDemo01.java
+++ git-test/git01/src/com/itheima/git/GitDemo01.java
@@ -4,4 +4,7 @@ public class GitDemo01 {
     public static void main(String[] args) {
         System.out.println("qqqqqqqqqqqqqqqq");
     }
+    public void test(){
+        System.out.println(123123);
+    }
 }
